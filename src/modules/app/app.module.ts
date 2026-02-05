1c458341bfd7f45932a149c9f57bd3cc43341d2d
antonio37tsarenko-boop <antonio37tsarenko@gmail.com>
2026-02-05 18:54:22 +0300

    commit app module changes

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { AppService } from "./app.service";
 import { AppController } from "./app.controller";
 import { ConfigModule } from "@nestjs/config";
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { ConfigModule } from "@nestjs/config";
       isGlobal: true,
       envFilePath: ".env",
     }),
+    AuthModule,
   ],
   providers: [AppService],
   controllers: [AppController],
